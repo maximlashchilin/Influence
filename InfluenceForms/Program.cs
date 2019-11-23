9ea5c14a3fb503a48074d40f429345c93568263d
Лащилин Максим Александрович <harddisk1598@gmail.com>
2019-11-23 18:41:38 +0300

    Work of winforms implementation

---
+++ InfluenceForms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Controller;
+
+namespace InfluenceForms
+{
+  static class Program
+  {
+    /// <summary>
+    /// Главная точка входа для приложения.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      new MainController().Start();
+    }
+  }
+}
