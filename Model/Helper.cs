90dd6b670536ca14ce3a614254f06a6781d60d51
Лащилин Максим Александрович <harddisk1598@gmail.com>
2020-01-09 04:01:42 +0300

    Helper and MenuView is changed

--- Model/Helper.cs
+++ Model/Helper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.IO;
 
 namespace Model
 {
@@ -16,15 +15,13 @@ namespace Model
     public event dPaintHandler PaintEvent;
 
     /// <summary>
-    /// Имя файла
-    /// </summary>
-    private const string HELP_FILE = "Help.txt";
-
-    /// <summary>
-    /// 
+    /// Текст справки
     /// </summary>
     private List<string> _helpText;
 
+    /// <summary>
+    /// Текст справки
+    /// </summary>
     public List<string> HelpText
     {
       get
@@ -33,39 +30,26 @@ namespace Model
       }
     }
 
+    /// <summary>
+    /// Инициализирует справку
+    /// </summary>
     public void Initialize()
     {
       string text = Resource.Help;
-      //_helpText = ReadTextFromFile(HELP_FILE);
       _helpText = SplitOnStrings(text);
       PaintEvent?.Invoke();
     }
 
+    /// <summary>
+    /// Разделяет текст на строки
+    /// </summary>
+    /// <param name="parText">Исходный текст</param>
+    /// <returns>Список строк текста</returns>
     private List<string> SplitOnStrings(string parText)
     {
       string[] strings = parText.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
      
       return strings.ToList<string>();
-    }
-
-    private List<string> ReadTextFromFile(string parFileName)
-    {
-      List<string> result = new List<string>();
-      using (StreamWriter writer = new StreamWriter(parFileName, true)) { writer.Close(); }
-      using (StreamReader reader = new StreamReader(parFileName))
-      {
-        string currentString = reader.ReadToEnd();
-        string[] lines = currentString.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-
-        foreach (string line in lines)
-        {
-          result.Add(line);
-        }
-
-        reader.Close();
-      };
-
-      return result;
     }
   }
 }
